f914e0bb489d9efd6a278d3b63f96d49acaa766e
menezesmalu <mlmv@cin.ufpe.br>
2018-04-21 18:51:31 -0300

    days of the week exercises

--- lista3diasdasemana.hs
+++ lista3diasdasemana.hs
@@ -1,20 +1,38 @@
 -- Implemente um tipo algébrico DiasSemana, começando do Domingo e indo até Sábado, 
 --instancie ele para Enum, Show, Ord e Eq e crie 3 funções: 
-data DiasSemana = Dom |Seg | Ter | Qua | Qui | Sex | Sab deriving (Show, Ord, Eq)
-diasUteis = [Seg , Ter , Qua , Qui , Sex ]
+data DiasSemana = Dom | Seg | Ter | Qua | Qui | Sex | Sab deriving (Show, Ord, Eq)
 
 --Dada uma lista de dias da semana retorna os dias úteis ordenados;
- ordenaUteis :: [DiasSemana] -> [DiasSemana]
+ordenaUteis :: [DiasSemana] -> [DiasSemana]
+ordenaUteis [] = []
+ordenaUteis (d:ds)
+    | Seg <= d && d <= Sex =
+         ordenaUteis [y| y<- ds, y < d] ++ [d] ++ ordenaUteis [y | y <- ds, y >= d]
+    | otherwise = ordenaUteis ds
 
---Dada uma lista de tuplas (Dia da semana, data) e um dia da semana, retorna uma lista com somente as datas que sejam do mesmo dia da semana;
+--Dada uma lista de tuplas (Dia da semana, data) e um dia da semana, 
+--retorna uma lista com somente as datas que sejam do mesmo dia da semana;
 datasIguais :: [(DiasSemana,Int)] -> DiasSemana -> [Int]
---Recebe um dia da semana (referente ao primerio dia do mes) e retorna uma lista de tuplas com todos os dias do mês. Considere o mês como tendo 30 dias.
-imprimeMes :: DiasSemana -> [(Int, DiasSemana)]
+datasIguais [] x = []
+datasIguais ((a,b):ds) x
+    | a == x = [b] ++ datasIguais ds x
+    | otherwise = datasIguais ds x
 
--- Exemplo:
--- > ordenaUteis [Segunda, Segunda, Terca, Domingo, Quarta]
--- [Domingo, Segunda, Segunda, Terca, Quarta]
--- > datasIguais [(Sexta, 2),(Sabado, 3),(Quinta, 10),(Domingo, 25),(Quinta, 29)] Quinta
--- [10, 29]
--- > imprimeMes Terca
--- [(1, Terca), (2, Quarta), (3, Quinta), (4, Sexta), (5, Sabado), (6, Domingo), (7, Segunda), (8, Terca), (9, Quarta) etc...]
+--Recebe um dia da semana (referente ao primerio dia do mes) e retorna uma 
+--lista de tuplas com todos os dias do mês. Considere o mês como tendo 30 dias.
+suc :: DiasSemana -> DiasSemana
+suc Dom = Seg
+suc Seg = Ter
+suc Ter = Qua
+suc Qua = Qui 
+suc Qui = Sex 
+suc Sex = Sab
+suc Sab = Dom
+
+imprimeMesSeq:: DiasSemana -> Int -> [(Int, DiasSemana)]
+imprimeMesSeq x y 
+    | y <= 30 =  [(y, x)] ++ imprimeMesSeq (suc x) (y+1)
+    | otherwise = []
+
+imprimeMes :: DiasSemana -> [(Int, DiasSemana)]
+imprimeMes x =  imprimeMesSeq x 1
